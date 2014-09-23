2d2f8056bfbcb8ddd01f87cc9bde9c05ce620289
Eugene1311 <ef7@mail.ru>
2014-09-24 02:51:13 +0400

    1

--- js/script.js
+++ js/script.js
@@ -4,21 +4,22 @@ if ($ === undefined) {
    console.log('Версия jQuery: ' + $.fn.jquery);
 }
 $(function () {
+		
     $('nav ul li a').click(function () {
         //$('nav').fadeTo("slow" , 0.8);
         $('nav ul li a').removeClass("active");
-        $(this).toggleClass("active");
+        $(this).addClass("active");
         var aId = $(this).attr("href");
-        if (aId != "#") {
-            $('nav').slideUp("slow");
-        }
-        $(window).scroll(function () {
+        if (aId !== "#") {
+			//$(window).off(scroll);
+            $('nav').slideUp('slow');
+				
+		}
+    });
+	$(window).on();
+	$(window).scroll(function () {
         //alert("Вы прокрутили содержимое данного окна.");
-        $('nav').slideDown("slow");   
-    });
-    });
-    /*$('nav').mousedown(function () {
-        $(this).fadeTo("slow" , 0.8);    
-    });*/
-    
+        $('nav').slideDown("slow");
+	});
+	
 });
